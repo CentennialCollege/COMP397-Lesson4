4f4b33b573c7a0b2c771ddb52c128b74f35ec368
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-02 16:40:30 -0400

    created Scene class and updated Game.ts and Menu.ts

--- COMP397-Lesson4/Scripts/core/game.js
+++ COMP397-Lesson4/Scripts/core/game.js
@@ -1,5 +1,4 @@
-﻿/// <reference path="../config/config.ts" />
-
+/// <reference path="../config/config.ts" />
 /// <reference path="../typings/jquery/jquery.d.ts" />
 /// <reference path="../typings/stats/stats.d.ts" />
 /// <reference path="../typings/createjs-lib/createjs-lib.d.ts" />
@@ -7,50 +6,37 @@
 /// <reference path="../typings/tweenjs/tweenjs.d.ts" />
 /// <reference path="../typings/soundjs/soundjs.d.ts" />
 /// <reference path="../typings/preloadjs/preloadjs.d.ts" />
-
 /// <reference path="../objects/label.ts" />
 /// <reference path="../objects/button.ts" />
 /// <reference path="../objects/scene.ts" />
-
 /// <reference path="../states/menu.ts" />
-
-
 // GLOBAL GAME FRAMEWORK VARIABLES
-var canvas: HTMLElement;
-var stage: createjs.Stage;
-var stats: Stats;
-var state: number;
-var currentState: objects.Scene; // alias for our current state
-
+var canvas;
+var stage;
+var stats;
+var state;
+var currentState; // alias for our current state
 // GAME OBJECTS
-var menu: states.Menu;
-
-
-function init():void {
+var menu;
+function init() {
     canvas = document.getElementById("canvas"); // reference to canvas element
     stage = new createjs.Stage(canvas); // passing canvas to stage
     stage.enableMouseOver(20); // enable mouse events
     createjs.Ticker.setFPS(60); // set frame rate to 60 fps
     createjs.Ticker.on("tick", gameLoop); // update gameLoop every frame
     setupStats(); // sets up our stats counting
-
     state = config.MENU_STATE;
     changeState();
 }
-
 // Main Game Loop
-function gameLoop(event: createjs.Event): void {
+function gameLoop(event) {
     stats.begin(); // start counting
-
-
     currentState.update();
     stage.update(); // redraw/refresh stage every frame
-
     stats.end(); // stop counting
 }
-
 // Setup Game Stats
-function setupStats():void {
+function setupStats() {
     stats = new Stats();
     stats.setMode(0); // shows fps
     stats.domElement.style.position = "absolute";
@@ -58,22 +44,15 @@ function setupStats():void {
     stats.domElement.style.top = "0px";
     document.body.appendChild(stats.domElement);
 }
-
-
-
-
-
 // state machine prep
-function changeState(): void {
+function changeState() {
     // Launch various scenes
-
     switch (state) {
         case config.MENU_STATE:
             // show the menu scene
             menu = new states.Menu();
             console.log(menu);
             currentState = menu;
-
             break;
         case config.PLAY_STATE:
             // show the play scene
@@ -82,7 +61,6 @@ function changeState(): void {
             // show the game over scene
             break;
     }
-
     currentState.start();
 }
- 
+//# sourceMappingURL=game.js.map
